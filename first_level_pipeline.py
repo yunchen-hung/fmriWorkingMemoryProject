cc206b9ac255061c67f903a0df0db28628d24fe3
jancayan <judelancayan@gmail.com>
2025-01-31 14:05:34 -0800

    save to file + small bugfixes

--- first_level_pipeline.py
+++ first_level_pipeline.py
@@ -17,14 +17,14 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 
 
-def extract_beta_weights(subject_id = None, task_type = 'colorwheel',  n_runs=1):
+def extract_beta_weights(subject_id = None, task_type = 'colorwheel',  num_run=1):
     """
     extract_beta_weights runs first level analysis on each of the subjects, 
     storing their weights for each task type and run in a dictionary.
 
     subject_id: subject ID 
     task_type: colorWheel or sameDifferent 
-    n_runs: max 4 runs, use less when testing
+    num_run: max 4 runs, use less when testing
     """
     #parameters
     tr = 2.0 
@@ -39,10 +39,9 @@ def extract_beta_weights(subject_id = None, task_type = 'colorwheel',  n_runs=1)
     # choose whatever confounds you want to include
     interested_confounds = ["rot_x", "trans_x", "white_matter", "csf"]
 
-    for num_run in range(n_runs):
-        preproc_path = f"~/teams/a05/group_1_data/fmriprep/sub-{subject_id}/func/sub-{subject_id}_task-{task_type}**{num_run + 1}**desc-preproc_bold.nii.gz"
-        events_path = f"~/teams/a05/group_1_data/fmriprep/events/sub-{subject_id}_task-{task_type}_acq-multiband_run-{num_run+1}_events.tsv"
-        
+    preproc_path = f"~/teams/a05/group_1_data/fmriprep/sub-{subject_id}/func/sub-{subject_id}_task-{task_type}**{num_run}**desc-preproc_bold.nii.gz"
+    events_path = f"~/teams/a05/group_1_data/fmriprep/events/sub-{subject_id}_task-{task_type}_acq-multiband_run-{num_run}_events.tsv"
+    
         #load subject nii files
         run_img = image.load_img(preproc_path)
 
@@ -74,20 +73,28 @@ def extract_beta_weights(subject_id = None, task_type = 'colorwheel',  n_runs=1)
         beta_weights = fmri_glm.compute_contrast(task_type, output_type="effect_size")
     return beta_weights
 
+def save_beta(img, subject_id = None, task_type = 'colorwheel',  run_num=1):
+    output_dir = Path("~/private/betas/")
+    output_dir.mkdir(exist_ok=True, parents=True)
+    img.to_filename(
+        output_dir / f"beta_{subject_id}_{task_type}_{run_num}.nii.gz"
+    )
 
 def main():
     top29Subjects = [103, 105, 106, 110, 112, 113, 115, 124, 127, 130, 
                     131, 133, 138, 142, 143, 145, 157, 159, 161, 165, 
                     173, 176, 177, 183, 187, 195, 200, 207, 208]
-    taskType = ['colorWheel', 'sameDifferent']
+    taskType = ['colorwheel', 'samedifferent']
+    num_runs =[1, 2, 3, 4]
     #all_subject_features = []
     #all_subject_labels = []
     
     for subjID in top29Subjects:
         for task in taskType:
-            beta_weights = extract_beta_weights(subject_id=subjID, task_type=task)
-
-            #save beta weights
-            beta_weights.to_file()
-            #all_subject_features.append(beta_weights)
-            #all_subject_labels.append(task)
+            for run in num_runs:
+                beta_weights = extract_beta_weights(subject_id=subjID, task_type=task, run)
+    
+                #save beta weights
+                save_beta(beta_weights, subjID, task)
+                #all_subject_features.append(beta_weights)
+                #all_subject_labels.append(task)
