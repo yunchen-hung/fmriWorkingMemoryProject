3a178ff3997eb59508d7f04b8685500cff183dc6
Jennifer <yunchen.hung@gmail.com>
2025-02-26 09:03:16 -0800

    figured out 3d brain but not sure if correct

--- SVM/.ipynb_checkpoints/svm-checkpoint.py
+++ SVM/.ipynb_checkpoints/svm-checkpoint.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import numpy as np
+import glob
+
+#data import
+from dataExtraction import load_beta_data
+
+#LIBSVM
+from libsvm.svmutil import *
 
 #nilearn imports
-from nilearn import plotting, image, interfaces
+import nilearn
+from nilearn import plotting, image, interfaces, maskers
 from nilearn.image import mean_img
 from nilearn.plotting import plot_anat, plot_img, plot_stat_map, show, plot_design_matrix
 from nilearn.glm import threshold_stats_img
 from nilearn.glm.first_level import FirstLevelModel, make_first_level_design_matrix
 from nilearn.reporting import get_clusters_table
-from nilearn.input_data import NiftiMasker
+import nibabel as nib
+from nilearn.maskers import NiftiMasker
 
 #sklearn imports
 from sklearn.svm import SVC
@@ -16,19 +25,19 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 
-from first_level_pipeline import extract_beta_weights
-
-def train_evaluate_model(X, y):
+def train_svmLight(X, y):
+    mask_img = nilearn.datasets.load_mni152_brain_mask(resolution=2, threshold=0.2)
+    masker = NiftiMasker(mask_img=mask_img, memory="nilearn_cache", memory_level=1).fit()
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=42)
 
     # SVM classifier
-    svm_model = SVC(kernel='linear')
-    svm_model.fit(X_train, y_train)
-    y_pred = svm_model.predict(X_test)
+    svm_model = svm_train(y_train, X_train)
+    svm_save_model('svm_beta_model', svm_model)
+    y_pred, accuracy, _ = svm_predict(y_test, X_test, svm_model)
+    
 
     # Evaluate performance
-    accuracy = accuracy_score(y_test, y_pred)
     print("Model Accuracy: " + str(accuracy))
 
     # Confusion matrix
@@ -37,25 +46,31 @@ def train_evaluate_model(X, y):
     disp.plot()
     show()
 
-    return accuracy
+    #print 3D model of predicted brain
+    support_vectors_dict = svm_model.get_SV()
+    max_feature_index = max(max(sv.keys()) for sv in support_vectors_dict)
+    
+    support_vectors = np.zeros((len(support_vectors_dict), max_feature_index))
+    
+    for i, sv in enumerate(support_vectors_dict):
+        for key, value in sv.items():
+            support_vectors[i, key - 1] = value 
+    
+    #get dual coefficients (alpha * y)
+    dual_coefficients = np.array(svm_model.get_sv_coef()).flatten()
+    
+    #compute weight vector
+    weight_1d = np.sum(support_vectors.T * dual_coefficients, axis=1)
+    
+    weights_3d = masker.inverse_transform(weight_1d)
+    plotting.plot_stat_map(weights_3d, vmax=2, alpha=0.5, title=f"Weighted Brain")
+
+    return svm_model
 
 def main():
     top29Subjects = [103, 105, 106, 110, 112, 113, 115, 124, 127, 130, 
                     131, 133, 138, 142, 143, 145, 157, 159, 161, 165, 
                     173, 176, 177, 183, 187, 195, 200, 207, 208]
-    taskType = ['colorWheel', 'sameDifferent']
-    all_subject_features = []
-    all_subject_labels = []
-    
-    for subjID in top29Subjects:
-        for task in taskType:
-            beta_weights = extract_beta_weights(subject_id=subjID, task_type=task)
-            all_subject_features.append(beta_weights)
-            all_subject_labels.append(task)
-
-    X = np.array(all_subject_features)
-    y = np.array(all_subject_labels)
-
-    # Train and evaluate the model
-    accuracy_score = train_evaluate_model(X, y)
-    print(accuracy_score)
+    X, y = load_beta_data(subjects, 'nonConfound')
+    y = [1 if task == 'colorwheel' else 0 for task in y]
+    train_model(X, y)
