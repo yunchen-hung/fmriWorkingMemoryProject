5166b3172668b67be05b829a162defdc7b3d49c2
Jennifer <yunchen.hung@gmail.com>
2025-02-26 08:04:24 -0800

    cant get the 3d brain

--- preprocessingPipelines/dataExtraction.py
+++ preprocessingPipelines/dataExtraction.py
@@ -1,48 +1,56 @@
 import pandas as pd
 import numpy as np
 import glob
-import matplotlib.pyplot as plt
 
 #nilearn imports
-from nilearn import plotting, image, interfaces
+import nilearn
+from nilearn import plotting, image, interfaces, maskers
 from nilearn.image import mean_img
 from nilearn.plotting import plot_anat, plot_img, plot_stat_map, show, plot_design_matrix
 from nilearn.glm import threshold_stats_img
 from nilearn.glm.first_level import FirstLevelModel, make_first_level_design_matrix
 from nilearn.reporting import get_clusters_table
-import nilearn
 import nibabel as nib
+from nilearn.maskers import NiftiMasker
 
-top29Subjects = [103, 105, 106, 110, 112, 113, 115, 124, 127, 130, 
-                    131, 133, 138, 142, 143, 145, 157, 159, 161, 165, 
-                    173, 176, 177, 183, 187, 195, 200, 207, 208]
-newSubjects =  [#107AB, 
-                109, 117, 140, 147, 172, #173AB, 
-                178, 180, 181, 182, 188]
+subjects = [103, 105, 106, 110, 112, 113, 115, 124, 127, 130, 
+            131, 133, 138, 142, 143, 145, 157, 159, 161, 165, 
+            173, 176, 177, 183, 187, 195, 200, 207, 208, 109,
+            117, 140, 147, 172, 178, 180, 181, 182, 188]
 
-def load_beta_data(top29Subjects):
+def load_beta_data(subjects, data_type):
+    """
+    subjects: a 1-D array of subject IDs 
+    data_type: 'confound' or 'nonConfound', depending on what you are training with
+
+    This function uses the MNI brain mask to create a uniform mask for all brain images
+    """
     taskType = ['colorwheel', 'samedifferent']
     num_runs = [1, 2, 3, 4]
     
     X, y = [], []
 
-    for subjID in top29Subjects:
+    for subjID in topSubjects:
         for task in taskType:
             for run in num_runs:
                 try:
-                    file_path = f"~/teams/a05/group_1_data/nonConfoundBeta/beta_{subjID}_{task}_{run}.nii.gz"
-    
-                    # loading image
+                    file_path = f"~/teams/a05/group_1_data/{data_type}Beta/beta_{subjID}_{task}_{run}.nii.gz"
+                    
                     beta_img = nib.load(str(file_path))
-    
+                    
                     # getting the data as an array, then flattening to 1D feature vector for model training
-                    beta_data = beta_img.get_fdata().flatten()
+                    mask_img = nilearn.datasets.load_mni152_brain_mask(resolution=2, threshold=0.2)
+                    masker = NiftiMasker(mask_img=mask_img, memory="nilearn_cache", memory_level=1).fit()
+
+                    #transform the subject data to get the brain voxels
+                    beta_data = masker.transform(beta_img).flatten()
+                
                     X.append(beta_data)
     
                     # appending the task category to y
-                    y.append((task))
+                    y.append(task)
                 except:
                     #if there isn't a specific run, i.e. run 4
                     continue
-    
+
     return np.array(X), y
